097310df58761e16c777b606bccc4ec5c9c0afc7
Daniil Starov <jazzzydan@hotmail.com>
2024-02-16 17:38:10 +0200

    Initial commit

---
+++ src/main/java/AverageOfNumbers.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            count = count + 1;
+            sum = sum + number;
+        }
+        double average = (double) sum / count;
+        System.out.println("Average of the numbers: " + average);
+    }
+}
